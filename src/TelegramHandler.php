71982b76933329d04a9d1d10cfb94df35598f14e
Tajul Asri <mtajulasri@gmail.com>
2018-02-12 11:46:46 +0800

    send formatted message

--- src/TelegramHandler.php
+++ src/TelegramHandler.php
@@ -1,5 +1,5 @@
 <?php
-namespace rahimi\TelegramHandler;
+namespace Cet\TelegramHandler;
 
 use Exception;
 use Monolog\Formatter\LineFormatter;
@@ -13,109 +13,172 @@ use Monolog\Logger;
  * into telegram using it's API.
  *
  * @author Moein Rahimi <m.rahimi2150@gmail.com>
+ * @author M tajul Asri <mtajulasri@gmai.com>
  */
 
 class TelegramHandler extends AbstractProcessingHandler
 {
 
+    /**
+     * @var mixed
+     */
     private $token;
+    /**
+     * @var mixed
+     */
     private $channel;
+    /**
+     * @var mixed
+     */
     private $dateFormat;
-    const host = 'https://api.telegram.org/bot';
+
+    /**
+     * @var mixed
+     */
+    private $timeZone;
+
+    /**
+     * @var mixed
+     */
+    private $response;
+
+    /**
+     * @var array
+     */
+    private $emojiMapping = [
+        Logger::DEBUG     => '🚧',
+        Logger::INFO      => '‍🗨',
+        Logger::NOTICE    => '🕵',
+        Logger::WARNING   => '⚡️',
+        Logger::ERROR     => '🚨',
+        Logger::CRITICAL  => '🤒',
+        Logger::ALERT     => '👀',
+        Logger::EMERGENCY => '🤕',
+    ];
 
     /**
      * getting token a channel name from Telegram Handler Object.
      *
-     * @param string $token Telegram Bot Access Token Provided by BotFather
-     * @param string $channel Telegram Channel userName
-     * @param string|int @level Debug level of Logged Event
-
+     * @param string     $token   Telegram Bot Access Token Provided by BotFather
+     * @param string     $channel Telegram Channel userName
+     * @param string|int @level   Debug level of Logged Event
      */
 
-    public function __construct($token, $channel,$timeZone = 'UTC',$dateFormat='F j, Y, g:i a')
+    public function __construct($token, $channel, $timeZone = 'UTC', $dateFormat = 'F j, Y, g:i a')
     {
 
-        if (!extension_loaded('curl')) {
-            throw new Exception('curl is needed to use this library');
-        }
-        $this->token   = $token;
+        $this->token = $token;
         $this->channel = $channel;
         $this->dateFormat = $dateFormat;
-        date_default_timezone_set($timeZone);
+
+        $this->getTimezone();
     }
 
     /**
      * format the log to send
-     * @param $record[] log data
+     * @param  $record[] log data
      * @return void
      */
     public function write(array $record)
     {
         $format = new LineFormatter;
         $context = $record['context'] ? $format->stringify($record['context']) : '';
-        $date =  date($this->dateFormat);
-        $message = $date . PHP_EOL . $this->getEmoji($record['level']) . ' ' . $record['message'] . $context;
+        $date = date($this->dateFormat);
+        $message = $date . PHP_EOL . $this->emojiMapping[$record['level']] . ' ' . $record['formatted'] . $context;
+
         $this->send($message);
 
     }
 
     /**
      *    send log to telegram channel
-     *    @param string $message Text Message
-     *    @return void
-     *
+     * @param  string $message Text Message
+     * @return void
      */
-    public function send($message)
+    public function send($message, array $options = [])
     {
-      try{
-        $ch = curl_init();
-        $url = self::host . $this->token . "/SendMessage";
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
-            'text'    => $message,
-            'chat_id' => $this->channel,
-        )));
-         $result = curl_exec($ch);
-         $result = json_decode($result,1);
-         if($result['ok'] === false){
-            echo  'telegram api response : ' .$result['description'];
-         }
-       }catch(Exception $error){
-         echo $error;
-       }
+        try {
+            $ch = curl_init();
+            $url = $this->getEndpoint() . $this->token . "/SendMessage";
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
+                'text'    => $message,
+                'chat_id' => $this->channel,
+                $options,
+            )));
+            $result = curl_exec($ch);
+            $result = json_decode($result, 1);
+            if ($result['ok'] === false) {
+                echo 'telegram api response : ' . $result['description'];
+            }
+        } catch (Exception $error) {
+            echo $error;
+        }
     }
 
     /**
-     * make emoji for log events
-     * @return array
-     *
+     * set timezone
+     * @param $timeZone
      */
-    protected function emojiMap()
+    public function setTimezone($timeZone = null)
     {
-        return [
-            Logger::DEBUG     => '🚧',
-            Logger::INFO      => '‍🗨',
-            Logger::NOTICE    => '🕵',
-            Logger::WARNING   => '⚡️',
-            Logger::ERROR     => '🚨',
-            Logger::CRITICAL  => '🤒',
-            Logger::ALERT     => '👀',
-            Logger::EMERGENCY => '🤕',
-        ];
+        $this->timeZone = $timeZone;
     }
 
     /**
-     * return emoji for given level
-     *
-     * @param $level
+     * get timezone
+     * @return mixed
+     */
+    public function getTimezone()
+    {
+        return $this->timeZone;
+    }
+
+    /**
+     * set date format
+     * @param $format
+     */
+    public function setDateFormat($format = null)
+    {
+        $this->dateFormat = $format;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateformat()
+    {
+        return $this->dateFormat;
+    }
+
+    /**
+     * set default timezone
+     */
+    protected function setDefaultTimezoneFormat()
+    {
+        return date_default_timezone_set($this->timeZone);
+    }
+
+    /**
+     * check for curl extension
+     * @return Exception
+     */
+    protected function curlExtensionInstalled()
+    {
+        if (!extension_loaded('curl')) {
+            throw new Exception('curl is needed to use this library');
+        }
+    }
+
+    /**
+     * telegram endpoint
      * @return string
      */
-    protected function getEmoji($level)
+    protected function getEndpoint()
     {
-        $levelEmojiMap = $this->emojiMap();
-        return $levelEmojiMap[$level];
+        return 'https://api.telegram.org/bot';
     }
 
 }
